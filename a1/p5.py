60b75d9aa9380ee7539a2f9bfdff9dbc841cd2dc
David Zhou <david.zhou.0110@gmail.com>
2025-09-18 15:08:55 +0800

    Completed A1

--- a1/p5.py
+++ a1/p5.py
@@ -7,13 +7,18 @@ def astar_search(problem):
     heuristic = problem['heuristic']
 
     start_h = heuristic[start]
-    pq = [(start_h, 0, start, [start])]
+    # One problem I had here is that when two f_cost are tied, the tiebreaker would be g_cost.
+    # However, it seems that the tiebreaker should be the insertion order instead. That is why
+    # I am using a counter here to track insertion order
+    counter = 0
+    pq = [(start_h, counter, 0, start, [start])]
+    counter += 1
     visited = set()
     exploration = []
     final_path = []
 
     while pq:
-        f_cost, g_cost, node, path = heapq.heappop(pq)
+        f_cost, insertion_order, g_cost, node, path = heapq.heappop(pq)
 
         if node in visited:
             continue
@@ -32,7 +37,8 @@ def astar_search(problem):
                 neighbor_h_cost = heuristic[neighbor]
                 new_f_cost = new_g_cost + neighbor_h_cost
                 new_path = path + [neighbor]
-                heapq.heappush(pq, (new_f_cost, new_g_cost, neighbor, new_path))
+                heapq.heappush(pq, (new_f_cost, counter, new_g_cost, neighbor, new_path))
+                counter += 1
 
     solution_lines = [" ".join(exploration), " ".join(final_path)]
     solution = '\n'.join(solution_lines)
